7f1e41477663c4ff740f10677621e1ceb76144d6
Jacob Percy <jacobpercy09@gmail.com>
2025-06-08 22:11:46 -0400

    webssite needs formatting and polishing but is good

--- tm/app.py
+++ tm/app.py
@@ -10,13 +10,21 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def index():
     if request.method == "POST":
         file = request.files["image"]
+
+        for filename in os.listdir(UPLOAD_FOLDER):
+            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
         img_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(img_path)
+
         turtle_code = run_turtle_script(headless=True, image_path=img_path)
 
-        return render_template("result.html", code=turtle_code)
+        return render_template("result.html", turtle_code=turtle_code)
 
     return render_template("upload.html")
+
 
 @app.route("/image")
 def image():
